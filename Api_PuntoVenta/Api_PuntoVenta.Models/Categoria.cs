a2562dc662256471b1c848c378e01898a59265cb
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-07-19 18:23:37 -0600

    entrega#4
    entregable#4

---
+++ Api_PuntoVenta/Api_PuntoVenta.Models/Categoria.cs
@@ -0,0 +1,325 @@
+﻿using AccesoDatos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_PuntoVenta.Models
+{
+    [ValidateNever]
+    public class Categoria : IOrdenConsulta
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+
+
+        #endregion ============================= PROPIEDADES =============================
+
+        public Respuesta Actualizar()
+        {
+            Respuesta miRespuesta = new Respuesta();
+            try
+            {
+                //Cantidad de registros afectados por la consulta 
+                string consulta = string.Empty;
+
+                if (tipoconsulta == 1)
+                {
+                    consulta = $@"
+                                   UPDATE T_Categoria SET nombre = '{this.nombre}' WHERE id = {id};
+ 
+                                   {miAuditoria.ObtenerAuditoria()}
+                                   
+                                   SELECT 2;";
+                }
+
+                else if (tipoconsulta == 2)
+                {
+
+                    consulta = $@"UPDATE T_Categoria SET estado = '{this.estado}' WHERE id = '{this.id}'
+                                            {miAuditoria.ObtenerAuditoria()}                                
+                                            SELECT 2;";
+                }
+
+
+
+
+                miRespuesta = transaccion(consulta);
+
+                if (miRespuesta.codigoError == -3)
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Realizar la Acción!";
+                }
+                else if (miRespuesta.codigoError == 2)
+                {
+                    miRespuesta.resultado = true;
+                    miRespuesta.mensaje = "Actualizado con exito!";
+                }
+                else
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Realizar la acción!";
+                }
+            }
+            catch (Exception es)
+            {
+                miRespuesta.mensaje = "Ocurrio un error al realizar la acción.";
+                miRespuesta.resultado = false;
+                miRespuesta.codigoError = -2;
+            }
+
+            return miRespuesta;
+        }
+
+        public Respuesta ConsultaMasiva()
+        {
+            //Luego instanciamos la clase conexion para su suso
+            AccesoDatos.Conexion objDatos = new AccesoDatos.Conexion();
+            Respuesta miRespuesta = new Respuesta();
+
+            try
+            {
+                List<Categoria> objEncontrado = null;
+                string consulta = "";
+
+                if (objDatos.AbrirConexion())
+                {
+                    if (tipoconsulta == 1)
+                    {
+                        consulta = $@"SELECT id, nombre, fecha_registro, estado 
+                                    FROM T_Categoria 
+                                    FOR JSON PATH";
+                    }
+
+                    objEncontrado = JsonConvert.DeserializeObject<List<Categoria>>(objDatos.HacerSelectJSONPATH(consulta));
+
+
+                    if (objEncontrado != null)
+                    {
+                        miRespuesta.resultado = true;
+                        miRespuesta.mensaje = "Exito";
+                        miRespuesta.codigoError = 2;
+                        miRespuesta.objeto = objEncontrado;
+
+                    }
+                    else
+                    {
+                        miRespuesta.resultado = false;
+                        miRespuesta.mensaje = "Sin Datos";
+                        miRespuesta.codigoError = -1;
+                        miRespuesta.objeto = null;
+                    }
+
+                    //Se cierra la conexion
+                    objDatos.CerrarConexion();
+                }
+                else
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Abrir la Conexion.";
+                    miRespuesta.codigoError = -2;
+                    miRespuesta.objeto = null;
+                }
+                //El objeto objDatos es destruido
+                objDatos = null;
+
+
+            }
+            catch (Exception es)
+            {
+                objDatos.CerrarConexion();
+                miRespuesta.resultado = false;
+                miRespuesta.mensaje = "Error en el Servicio, Intente de Nuevo.";
+                miRespuesta.codigoError = -3;
+                miRespuesta.objeto = null;
+            }
+
+
+            return miRespuesta;
+        }
+
+        public Respuesta Guardar()
+        {
+            Respuesta miRespuesta = new Respuesta();
+            try
+            {
+                //Cantidad de registros afectados por la consulta 
+                string consulta = string.Empty;
+
+                if (tipoconsulta == 1)
+                {
+                    consulta = $@"IF((SELECT COUNT(*) FROM T_Categoria WHERE nombre = '{nombre}') <= 0)
+	                                BEGIN
+                                        
+                                        DECLARE @id as int = (SELECT ISNULL(MAX(id), 0) + 1 from T_Categoria);
+
+		                                INSERT INTO T_Categoria (id, nombre)
+		                                VALUES(@id, '{nombre}'); 
+
+                                         {miAuditoria.ObtenerAuditoria()}
+
+                                          SELECT 2;
+
+
+	                                END
+                                ELSE
+	                                BEGIN
+                                        SELECT -50;
+                                    END
+
+                                           ";
+                }
+
+
+
+
+                miRespuesta = transaccion(consulta);
+
+                if (miRespuesta.codigoError == -3)
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Realizar la Acción!";
+                }
+                else if (miRespuesta.codigoError == 2)
+                {
+                    miRespuesta.resultado = true;
+                    miRespuesta.mensaje = "Guardado con exito!";
+                }
+                else
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Realizar la acción!";
+                }
+            }
+            catch (Exception es)
+            {
+                miRespuesta.mensaje = "Ocurrio un error al realizar la acción.";
+                miRespuesta.resultado = false;
+                miRespuesta.codigoError = -2;
+            }
+
+            return miRespuesta;
+        }
+
+        public Respuesta Obtener()
+        {
+            //Luego instanciamos la clase conexion para su suso
+            AccesoDatos.Conexion objDatos = new AccesoDatos.Conexion();
+            Respuesta miRespuesta = new Respuesta();
+
+            try
+            {
+                Categoria objEncontrado = null;
+                string consulta = "";
+
+                if (objDatos.AbrirConexion())
+                {
+                    if (tipoconsulta == 1)
+                    {
+                        consulta = $@"SELECT id, nombre, fecha_registro, estado 
+                                    FROM T_Categoria
+                                    WHERE id = '{id}' 
+                                    FOR JSON PATH, WITHOUT_ARRAY_WRAPPER";
+                    }
+
+                    objEncontrado = JsonConvert.DeserializeObject<Categoria>(objDatos.HacerSelectJSONPATH(consulta));
+
+
+                    if (objEncontrado != null)
+                    {
+                        miRespuesta.resultado = true;
+                        miRespuesta.mensaje = "Exito";
+                        miRespuesta.codigoError = 2;
+                        miRespuesta.objeto = objEncontrado;
+
+                    }
+                    else
+                    {
+                        miRespuesta.resultado = false;
+                        miRespuesta.mensaje = "Sin Datos";
+                        miRespuesta.codigoError = -1;
+                        miRespuesta.objeto = null;
+                    }
+
+                    //Se cierra la conexion
+                    objDatos.CerrarConexion();
+                }
+                else
+                {
+                    miRespuesta.resultado = false;
+                    miRespuesta.mensaje = "Error al Abrir la Conexion.";
+                    miRespuesta.codigoError = -2;
+                    miRespuesta.objeto = null;
+                }
+                //El objeto objDatos es destruido
+                objDatos = null;
+
+
+            }
+            catch (Exception es)
+            {
+                objDatos.CerrarConexion();
+                miRespuesta.resultado = false;
+                miRespuesta.mensaje = "Error en el Servicio, Intente de Nuevo.";
+                miRespuesta.codigoError = -3;
+                miRespuesta.objeto = null;
+            }
+
+
+            return miRespuesta;
+        }
+
+        #region ************************************** TRANSACTION **************************************
+        public Respuesta transaccion(string query)
+        {
+            Respuesta miRespuesta = new Respuesta();
+
+
+            Conexion objCnx = new Conexion();
+
+            if (objCnx.AbrirConexion())
+            {
+
+                string scalar =
+                    string.Format(@" BEGIN TRANSACTION
+                        BEGIN TRY
+                        BEGIN
+                        SET DATEFORMAT ymd
+
+                        {0}	         
+
+                        COMMIT; 
+                        END
+                        END TRY
+                        BEGIN CATCH
+                        ROLLBACK;
+                        Select -3
+                        END CATCH;", query);
+
+                miRespuesta.codigoError = objCnx.HacerHitScalar(scalar);
+
+                objCnx.CerrarConexion();
+            }
+
+            objCnx = null;
+
+            return miRespuesta;
+        }
+
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ Api_PuntoVenta/Api_PuntoVenta/Controllers/ProveedoresController.cs
@@ -0,0 +1,86 @@
+﻿using Api_PuntoVenta.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_PuntoVenta.Controllers
+{
+    [Authorize]
+    [EnableCors("MyPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedoresController : ControllerBase
+    {
+
+        [EnableCors("MyPolicy")]
+        [HttpPost]
+        [Route("Listar")]
+        public Respuesta Listar(Proveedor request)
+        {
+            Respuesta miRespuesta = new Respuesta();
+
+            if (request != null)
+            {
+                miRespuesta = request.ConsultaMasiva();
+            }
+            else
+            {
+                miRespuesta.codigoError = -15;
+                miRespuesta.mensaje = "Ocurrio un error al conectar el servidor";
+                miRespuesta.resultado = false;
+            }
+
+            return miRespuesta;
+        }
+
+        [EnableCors("MyPolicy")]
+        [HttpPost]
+        [Route("Obtener")]
+        public Respuesta Obtener(Proveedor request)
+        {
+            Respuesta miRespuesta = new Respuesta();
+
+            if (request != null)
+            {
+                miRespuesta = request.Obtener();
+            }
+            else
+            {
+                miRespuesta.codigoError = -15;
+                miRespuesta.mensaje = "Ocurrio un error al conectar el servidor";
+                miRespuesta.resultado = false;
+            }
+
+            return miRespuesta;
+        }
+
+
+        [HttpPost]
+        [Route("Guardar")]
+        public Respuesta Guardar(Proveedor request)
+        {
+            Respuesta miRespuesta = new Respuesta();
+
+            if (request != null)
+            {
+                if (request.id > 0)
+                {
+                    miRespuesta = request.Actualizar();
+                }
+                else
+                {
+                    miRespuesta = request.Guardar();
+                }
+            }
+            else
+            {
+                miRespuesta.codigoError = -15;
+                miRespuesta.mensaje = "Ocurrio un error al conectar el servidor";
+                miRespuesta.resultado = false;
+            }
+
+            return miRespuesta;
+        }
+    }
+}
